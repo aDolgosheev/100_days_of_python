3d00fb08b130a83477932d6eccc326990d60e657
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2023-08-01 21:12:40 +0300

    update 2023.08.01

--- day_001/main.py
+++ day_001/main.py
@@ -1,1 +1,9 @@
-print("Hello, world")
+# print("Hello, world\nHello, world\nHello, world")
+# print("Hello" + " Sasha")
+# # print("Hello " + input("What's your name? "))
+# print(len(input("What's is your name? ")))
+
+print("Welcome to the band name generator!")
+city = input("Which city did you grow up? ")
+pet = input("What is the name of your first pet? ")
+print("Your band name could be " + city + " " + pet)
